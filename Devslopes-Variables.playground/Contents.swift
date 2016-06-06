b4733bee7f34c46a264186274143182f04e2204a
Michael Brown <mbrown@incipire.com>
2016-06-06 15:32:34 -0500

    fun at the playground

---
+++ Devslopes-Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myNumber = 15
+var anotherNumber = 5
+var sum = myNumber + anotherNumber
+var multiple = 3 * 15
+var difference = myNumber - anotherNumber
+var dividend = myNumber / anotherNumber
+var string = "yo", yetAnotherNumber = 32, anotherString = "Adrienne"
+let firstName = "Michael", middleName = "David", lastName = "Brown"
+var myDouble : Double
+var myInt : Int = 423
+var greeting = "Hello"
+var subject = "World"
+var sayHello = greeting + subject
+sayHello = "\(greeting) \(subject)."
